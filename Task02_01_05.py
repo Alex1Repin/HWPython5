ae350dc5d2af6ceabf876ff586d6e625656ffd5a
Alex <al.repin.a@yandex.ru>
2022-10-30 14:04:16 +0300

    add bot game

--- Task02_01_05.py
+++ Task02_01_05.py
@@ -39,20 +39,36 @@ def game(play_1, play_2):
     count = 0
     while count_candies > 0:
         if count == 0:
-            move = int(input (f'Твой ход {play_1}: ' ))
-            if move > max_move or move < 0:
-                move = int(input('Уверен? Попробуй ещё раз: '))
-            count_candies -= move
+            if play_1 == 'бот':
+                if count_candies > 28 and count_candies <= 2021:
+                    move = randint(1, 28)
+                    print(f'ходит {play_1}: {move} конфет')
+                else:
+                    move = count_candies
+                count_candies -= move
+            else:
+                move = int(input (f'Твой ход {play_1}: ' ))
+                if move > max_move or move < 0:
+                    move = int(input('Уверен? Попробуй ещё раз: '))
+                count_candies -= move
         if count_candies > 0:
             print(f'Осталось {count_candies} конфет')
             count = 1
         else:
             print(f'Ты выиграл {play_1}!')
         if count == 1:
-            move = int(input (f'Теперь твой ход {play_2} : ' ))
-            if move > max_move or move < 0:
-                move = int(input('Уверен? Попробуй ещё раз: '))
-            count_candies -= move
+            if play_2 == 'бот':
+                if count_candies > 28 and count_candies <= 2021:
+                    move = randint(1, 28)
+                    print(f'ходит {play_2}: {move} конфет')
+                else:
+                    move = count_candies
+                count_candies -= move
+            else:
+                move = int(input (f'Теперь твой ход {play_2} : ' ))
+                if move > max_move or move < 0:
+                    move = int(input('Уверен? Попробуй ещё раз: '))
+                count_candies -= move
         if count_candies > 0:
             print(f'Осталось {count_candies} конфет')
             count = 0
@@ -61,26 +77,9 @@ def game(play_1, play_2):
 
 players = who_plays()
 lots = lot(players[0], players[1])
-print(lots)
-#game(lots[0], lots[1])
+game(lots[0], lots[1])
 
-def bot(num):
-    if num > 28 and num <= 2021:
-        stride = randint(1, 28)
-    else:
-        stride = num
-    return stride
-#print(bot(17))
 
-def ii_bot(count_candies, move):
-    move_bot = None 
-    if count_candies == 2021:
-        move_bot = 20
-        return move_bot
-    elif count_candies == 2001:
-        print('Я не хочу играть')
-    else:
-        move_bot = 29-move
-        return move_bot
 #print(ii_bot(1001, 28))
 #who_plays()
+#print(lots)
